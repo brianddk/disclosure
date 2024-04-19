4ac2698475a98ea44d6061b2d382779a6f821cba
brianddk <brianddk@users.noreply.github.com>
2024-04-18 23:34:11 -0500

    CB-Disclosure # h1-2470858
      - New Coinbase disclosure at: https://hackerone.com/reports/2470858

---
+++ encrypted/template.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Keys for self and CB-Devs; GH user_ids:
+#   brianddk renanmav taycaldwell wbnns
+cd "$(dirname "$0")"
+key_ids=(            \
+  "835F0433A6D51860" \
+  "F11F27CB27C99543" \
+  "1D2438440FDE48B4" \
+  "954C0D74D2146C22" \
+  "AFAE33CB939EB312" \
+  "302EBEAEEB74A404" \
+  "3ABBE96D1430792F" )
+
+# Encrypt cleartext to encrypted
+_self="${0##*/}"
+_file="${_self%.sh}"
+infile="../cleartext/${_file}.md"
+outfile="../encrypted/${_file}.md.asc"
+
+if [ ! -e "${infile}" ]; then
+    touch "${infile}"
+fi
+
+# Build the GPG command to encrypt
+gpgcmd="gpg --homedir ../keystore -qea"
+for key in "${key_ids[@]}"; do
+    gpgcmd="${gpgcmd} -r ${key} --trusted-key ${key}"
+done
+
+# Encrypt
+$(${gpgcmd} --output ${outfile} -e ${infile})
